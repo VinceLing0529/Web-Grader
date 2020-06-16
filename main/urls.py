f0b7f2c8b5ccbc70e9a68f96726858a8ac47f289
VinceLing0529 <ldkxnb@gmail.com>
2020-06-16 08:35:51 -0700

    template tagging

--- main/urls.py
+++ main/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from . import views
 
+app_name = 'main'
+
 urlpatterns = [
     path('', views.index, name="index"),
     path('studentlogin/',views.studentlogin,name='studentlogin'),
-    path('teacherlogin/',views.studentlogin,name='teacherlogin'),
-
+    path('teacherlogin/',views.teacherlogin,name='teacherlogin'),
     path('signup/',views.signup,name='signup')
 ]
